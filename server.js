6b64ea69e6bee9c16d7618f5664105216f8396b3
etciramazanusta72 <etciramazanusta72@gmail.com>
2025-10-16 22:43:06 +0300

    Render için ilk sürüm

---
+++ server.js
@@ -0,0 +1,16 @@
+// server.js
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const app = express();
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+app.use(express.static(__dirname));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`✅ Server running on port ${port}`));
